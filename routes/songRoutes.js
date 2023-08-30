38c830b5d00e002be26373c7a77cd5299e49eb81
Bibin Prakash <bibin.dev2000@gmail.com>
2023-08-29 22:42:40 -0400

    modified cors

--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -22,7 +22,10 @@ const authOptions = {
 const app = express();
 // Set up CORS headers
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://spotify-apple-music-gamma.vercel.app/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
